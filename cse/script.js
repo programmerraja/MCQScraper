610643ad058717811a38f4e94dc125a60ac6b18f
programmerraja <boooathis123@gmail.com>
2021-02-04 14:50:54 +0530

    Added some MCQ

--- cse/script.js
+++ cse/script.js
@@ -2,6 +2,7 @@ link_tag=document.querySelectorAll("a")
 for(let i=0;i<link_tag.length;i++)
 {
 	//checking chilldren because it has img mean it is question else not 
+	console.log(link_tag[i].children[0])
 	if(link_tag[i].parentNode.parentNode.nodeName==="P" && !(link_tag[i].children[0]))
 	{
 		link_tag[i].parentNode.parentNode.style.display="none";
